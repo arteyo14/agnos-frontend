e78432ccccb4e466e187ed3b90edf55bfaaf9770
Yo.Pitchayakorn <pitchayakorn.s@clicknext.com>
2025-11-27 20:42:46 +0700

    feat: use environment variable for socket URL in socket.ts

--- lib/socket.ts
+++ lib/socket.ts
@@ -1,5 +1,7 @@
 import { io } from 'socket.io-client';
 
-export const socket = io('http://localhost:4000', {
+const socketURL = process.env.NEXT_PUBLIC_SOCKET_URL;
+
+export const socket = io(socketURL as string, {
   transports: ['websocket'],
 });
