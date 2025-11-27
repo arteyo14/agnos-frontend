479426c3e542c60bca555a6859d478095310c949
Yo.Pitchayakorn <pitchayakorn.s@clicknext.com>
2025-11-27 14:52:49 +0700

    add: change filename & delete unused file

--- app/(root)/page.tsx
+++
@@ -1,7 +0,0 @@
-import HomeView from '@/core/modules/home/ui/home-view';
-
-const HomePage = () => {
-  return <HomeView />;
-};
-
-export default HomePage;
--- core/modules/patient/patient-form/ui/patient-view.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-const PatientView = () => {
-  return <div>Patient View Component</div>;
-};
-
-export default PatientView;
